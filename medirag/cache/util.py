2c40689a8ab4f09dea1997c84767e401e76b1585
alvin <share.code@aol.com>
2024-08-15 14:12:51 -0700

    clean up

--- medirag/cache/util.py
+++ medirag/cache/util.py
@@ -8,38 +8,41 @@ from sentence_transformers import SentenceTransformer
 
 
 class SemanticCaching:
-    def __init__(self, model_name="sentence-transformers/all-mpnet-base-v2", dimension=768, cosine_threshold=0.7):
+    def __init__(self,
+                 model_name="sentence-transformers/all-mpnet-base-v2",
+                 dimension=768,
+                 cosine_threshold=0.7):
+
         self.model_name = model_name
         self.dimension = dimension
         self.cosine_threshold = cosine_threshold
-        self.index = None
-        self.storage_context = None
-        self.encoder = SentenceTransformer(self.model_name)
+        self.vector_store_index = None
+        self.storage_context = self.create_faiss_index()
 
     def create_faiss_index(self):
         index = faiss.IndexFlatIP(self.dimension)
         vector_store = FaissVectorStore(faiss_index=index)
-        self.storage_context = StorageContext.from_defaults(vector_store=vector_store)
-        return vector_store
+        storage_context = StorageContext.from_defaults(vector_store=vector_store)
+        return storage_context
 
     def build_index(self, documents):
         embed_model = HuggingFaceEmbedding(model_name=self.model_name)
         Settings.embed_model = embed_model
-        self.index = VectorStoreIndex.from_documents(documents, storage_context=self.storage_context)
+        self.vector_store_index = VectorStoreIndex.from_documents(documents, storage_context=self.storage_context)
 
     def save_cache(self, persist_dir="./cache"):
-        if self.index:
-            self.index.storage_context.persist(Path(persist_dir))
+        if self.vector_store_index:
+            self.vector_store_index.storage_context.persist(Path(persist_dir))
 
     def load_cache(self, persist_dir="./cache"):
         vector_store = FaissVectorStore.from_persist_dir(persist_dir)
         self.storage_context = StorageContext.from_defaults(vector_store=vector_store, persist_dir=persist_dir)
-        self.index = VectorStoreIndex(storage_context=self.storage_context)
+        self.vector_store_index = VectorStoreIndex(storage_context=self.storage_context)
 
     def ask(self, question: str) -> str:
-        if not self.index:
+        if not self.vector_store_index:
             raise ValueError("Index is not initialized. Please build or load an index first.")
         # TODO RAG
-        query_engine = self.index.as_query_engine()
+        query_engine = self.vector_store_index.as_query_engine()
         response = query_engine.query(question)
         return response
--- medirag/index/ingest.py
+++ medirag/index/ingest.py
@@ -7,35 +7,38 @@ from llama_index.vector_stores.faiss import FaissVectorStore
 
 
 class DailyMedIndexer:
-    def __init__(self, model_name="sentence-transformers/all-mpnet-base-v2", dimension=768):
+    def __init__(self,
+                 model_name="sentence-transformers/all-mpnet-base-v2",
+                 dimension=768):
+
         self.model_name = model_name
         self.dimension = dimension
-        self.index = None
-        self.storage_context = None
+        self.vector_store_index = None
+        self.storage_context = self.create_faiss_index()
 
     def create_faiss_index(self):
         index = faiss.IndexFlatIP(self.dimension)
         vector_store = FaissVectorStore(faiss_index=index)
-        self.storage_context = StorageContext.from_defaults(vector_store=vector_store)
-        return vector_store
+        storage_context = StorageContext.from_defaults(vector_store=vector_store)
+        return storage_context
 
     def build_index(self, documents):
         embed_model = HuggingFaceEmbedding(model_name=self.model_name)
         Settings.embed_model = embed_model
-        self.index = VectorStoreIndex.from_documents(documents, storage_context=self.storage_context)
+        self.vector_store_index = VectorStoreIndex.from_documents(documents, storage_context=self.storage_context)
 
     def save_index(self, persist_dir="./storage"):
-        if self.index:
-            self.index.storage_context.persist(Path(persist_dir))
+        if self.vector_store_index:
+            self.vector_store_index.storage_context.persist(Path(persist_dir))
 
     def load_index(self, persist_dir="./storage"):
         vector_store = FaissVectorStore.from_persist_dir(persist_dir)
         self.storage_context = StorageContext.from_defaults(vector_store=vector_store, persist_dir=persist_dir)
-        self.index = VectorStoreIndex(storage_context=self.storage_context)
+        self.vector_store_index = VectorStoreIndex(storage_context=self.storage_context)
 
     def retrieve(self, query, top_k=2):
-        if self.index:
-            retriever = self.index.as_retriever(similarity_top_k=top_k)
+        if self.vector_store_index:
+            retriever = self.vector_store_index.as_retriever(similarity_top_k=top_k)
             return retriever.retrieve(query)
         else:
             raise ValueError("Index is not initialized. Please build or load an index first.")

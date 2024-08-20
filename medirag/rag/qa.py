ca41bc842c98687bb318d39ab7bce2fcf1be4c71
alvin <share.code@aol.com>
2024-08-20 12:23:29 -0700

    Adding input and output guardrail

--- medirag/rag/qa.py
+++ medirag/rag/qa.py
@@ -3,6 +3,8 @@ from typing import Union, List, Optional
 import dspy
 from dsp import dotdict
 
+from medirag.guardrail.input import InputGuardrail
+from medirag.guardrail.output import OutputGuardrail
 from medirag.index.local import DailyMedIndexer
 
 
@@ -24,6 +26,12 @@ class DailyMedRetrieve(dspy.Retrieve):
 
 
 class GenerateAnswer(dspy.Signature):
+    """
+    You are an AI assistant designed to answer questions based on provided context:
+      - Ensure that your responses are accurate, safe, and reliable.
+      - Always remind users that you are not a doctor or medical advisor.
+      - Provide concise, clear, and relevant information, avoiding any form of diagnosis or treatment advice.
+    """
     context = dspy.InputField(desc="Contains relevant facts about drug labels")
     question = dspy.InputField()
     answer = dspy.OutputField(
@@ -33,6 +41,8 @@ class GenerateAnswer(dspy.Signature):
 class RAG(dspy.Module):
     def __init__(self, k: int = 3):
         super().__init__()
+        self.input_guardrail = dspy.TypedPredictor(InputGuardrail)
+        self.output_guardrail = dspy.TypedPredictor(OutputGuardrail)
 
         self.retrieve = dspy.Retrieve(k=k)
         self.generate_answer = dspy.ChainOfThought(GenerateAnswer)
@@ -40,5 +50,17 @@ class RAG(dspy.Module):
     def forward(self, question):
         context = self.retrieve(question).passages
 
+        in_gr = self.input_guardrail(user_input=question)
+
+        if in_gr.should_block == 'Yes':
+            return dspy.Prediction(context=question, answer="I'm sorry, I can't respond to that.")
+
         prediction = self.generate_answer(context=context, question=question)
+
+        out_gr = self.output_guardrail(user_input=question, bot_response=prediction.answer)
+
+        if out_gr.should_block == 'Yes':
+            return dspy.Prediction(context=context,
+                                   answer="I'm sorry, I don't have relevant information to respond to that.")
+
         return dspy.Prediction(context=context, answer=prediction.answer)
